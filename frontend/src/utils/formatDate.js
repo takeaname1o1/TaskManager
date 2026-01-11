75af20e2dec1b0e88274a33705232568e137f141
alp <jitenkr129152@gmail.com>
2026-01-12 03:10:45 +0530

    Remove frontend folder from backend branch

--- frontend/src/utils/formatDate.js
+++
@@ -1,2 +0,0 @@
-export const formatDate = (date) =>
-  new Date(date).toLocaleDateString();
--- frontend/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-// src/utils/logger.js
-const isDev = process.env.NODE_ENV === 'development';
-
-export const logger = {
-  info: (msg, data = '') => isDev && console.log(`%c[INFO]: ${msg}`, 'color: #00adef', data),
-  success: (msg, data = '') => isDev && console.log(`%c[SUCCESS]: ${msg}`, 'color: #00ff00', data),
-  error: (msg, error = '') => isDev && console.error(`[ERROR]: ${msg}`, error),
-};
